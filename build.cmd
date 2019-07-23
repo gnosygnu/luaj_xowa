abf46da8f0d0afa26ca681548c899ac6d351254c
gnosygnu <gnosygnu@gmail.com>
2019-07-22 20:57:41 -0400

    Luaj: Fix build warnings about 'unmappable character for encoding utf-8' [gnosygnu/xowa#502]

--- build.cmd
+++ build.cmd
@@ -1,1 +1,3 @@
+REM NOTE: must set encoding else "unmappable character for encoding utf-8"; https://stackoverflow.com/questions/21147974/set-a-system-property-with-ant
+SET ANT_OPTS="-Dfile.encoding=UTF8"
 ant -buildfile build.xml
